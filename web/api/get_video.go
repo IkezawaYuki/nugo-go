1a7a15a6a35de6e75529155d18b555c7b0c261db
yikezawa <y.ikezawa93@gmail.com>
2020-02-03 23:38:07 +0900

    mod

--- web/api/get_video.go
+++ web/api/get_video.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"firebase.google.com/go/auth"
+	"github.com/IkezawaYuki/nugo-go/middlewares"
+	"github.com/IkezawaYuki/nugo-go/models"
 	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/api/youtube/v3"
@@ -9,20 +12,40 @@ import (
 
 type VideoResponse struct{
 	VideoList *youtube.VideoListResponse `json:"video_list"`
+	IsFavorite bool `json:"is_favorite"`
 }
 
 func GetVideo() echo.HandlerFunc{
 	return func (c echo.Context) error{
 		yts := c.Get("yts").(*youtube.Service)
+		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
+		token := c.Get("auth").(*auth.Token)
 
 		videoID := c.Param("id")
+		isFavorite := false
+		if token != nil{
+			favorite := models.Favorite{}
+			isFavoriteNotFound := dbs.DB.Table("favorite").
+				Joins("INNER JOIN users ON users.id = favorites.user_id").
+				Where(models.User{UID: token.UID}).
+				Where(models.Favorite{VideoID: videoID}).
+				First(&favorite).
+				RecordNotFound()
+			logrus.Debug("isFavoriteNotFount: ", isFavoriteNotFound)
+			if !isFavoriteNotFound{
+				isFavorite = true
+			}
+		}
 
 		call := yts.Videos.List("id,snippet").Id(videoID)
 		res, err := call.Do()
 		if err != nil{
 			logrus.Fatalf("Error calling Youtube API: %v", err)
 		}
-		v := VideoResponse{VideoList:res}
+		v := VideoResponse{
+			VideoList:res,
+			IsFavorite: isFavorite,
+		}
 		return c.JSON(http.StatusOK, v)
 	}
 }
--- web/api/toggle_favorite_video.go
+++ web/api/toggle_favorite_video.go
@@ -1,6 +1,12 @@
 package api
 
-import "github.com/labstack/echo"
+import (
+	"firebase.google.com/go/auth"
+	"github.com/IkezawaYuki/nugo-go/middlewares"
+	"github.com/IkezawaYuki/nugo-go/models"
+	"github.com/labstack/echo"
+	"github.com/valyala/fasthttp"
+)
 
 type ToggleFavoriteVideoResponse struct{
 	VideoID string `json:"video_id"`
@@ -9,6 +15,32 @@ type ToggleFavoriteVideoResponse struct{
 
 func ToggleFavoriteVideo() echo.HandlerFunc{
 	return func(c echo.Context) error {
-		// todo 
+		dbs := c.Get("dbs").(*middlewares.DatabaseClient)
+		videoID := c.Param("id")
+		token := c.Get("auth").(*auth.Token)
+		user := models.User{}
+		if dbs.DB.Table("users").Where(models.User{
+			UID:       token.UID,
+		}).First(&user).RecordNotFound(){
+				user = models.User{UID:token.UID}
+				dbs.DB.Create(&user)
+		}
+		favorite := models.Favorite{}
+		isFavorite := false
+		if dbs.DB.Table("favorites").Where(models.Favorite{UserID: user.ID, VideoID: videoID}).
+			First(&favorite).RecordNotFound(){
+				favorite = models.Favorite{UserID: user.ID, VideoID: videoID}
+				dbs.DB.Create(&favorite)
+				isFavorite = true
+		}else{
+			dbs.DB.Delete(&favorite)
+		}
+
+		res := ToggleFavoriteVideoResponse{
+			VideoID:    videoID,
+			IsFavorite: isFavorite,
+		}
+
+		return c.JSON(fasthttp.StatusOK, res)
 	}
 }
